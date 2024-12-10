65c880b04bc39ee022ff9a92a947e5a384efd16d
kai@ST <chanthinker@foxmail.com>
2024-12-10 18:11:42 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -8,5 +8,6 @@ export const SITE = {
   "cmtURL":"https://cmt.testdog.cn",
   "cmtJs":"https://unpkg.com/@waline/client@2.15.8/dist/waline.js",
   "cmtCss":"https://unpkg.com/@waline/client@3.3.2/dist/waline.css",
-  "pv":true
+  "pv":true,
+  "buyMeCoffeeId":"thinkerchan"
 }
