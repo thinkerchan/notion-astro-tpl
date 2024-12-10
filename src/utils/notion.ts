4c77411f4f8d62a6501bbdc8c4cecd267e0b8987
kai@ST <chanthinker@foxmail.com>
2024-12-10 17:30:20 +0800

    update

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -22,19 +22,22 @@ async function fetchAppsFromNotion() {
       response.results.map(async (page: any) => {
         const pageId = page.id;
         const pageBlocks = await notion.blocks.children.list({ block_id: pageId });
+        const props = page.properties
+
         return {
           id: pageId,
-          title: page.properties.title.title[0]?.plain_text,
-          desc: page.properties.desc.rich_text[0]?.plain_text,
-          icon: page.properties.icon.files[0]?.type === 'external' ? page.properties.icon.files[0].external.url : page.properties.icon.files[0]?.file.url,
-          link: page.properties.link.url,
-          tags: page.properties.tags.multi_select.map((tag: any) => {
+          title: props.title.title[0]?.plain_text,
+          desc: props.desc.rich_text[0]?.plain_text,
+          icon: props.icon.files[0]?.type === 'external' ? props.icon.files[0].external.url : props.icon.files[0]?.file.url,
+          link: props.link.url,
+          tags: props.tags.multi_select.map((tag: any) => {
             return {
               name: tag.name,
               color: tag.color
             }
           }) || [],
           content: formatter(pageBlocks.results),
+          date: page.created_time? new Date(page.created_time).toLocaleDateString() : '',
         };
       })
     );
